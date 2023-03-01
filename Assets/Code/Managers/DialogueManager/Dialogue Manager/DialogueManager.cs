8138f329719e669c9d6c7e170f5c80ac206a1bc2
Pandoradoomer <44462289+Pandoradoomer@users.noreply.github.com>
2023-03-01 15:35:24 +0000

    [LO-84] Dialogue Manager Setup + Data
    Created Dialogue Manager
    Imported and created font assets for some fonts.
    Created Scriptable Objects for the dialogue system and populated a short array of them.

--- Assets/Code/Managers/DialogueManager/Dialogue Manager/DialogueManager.cs
+++ Assets/Code/Managers/DialogueManager/Dialogue Manager/DialogueManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
