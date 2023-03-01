2370c30a31fd7ddf676ed71300cb0cdcbbd03b46
Pandoradoomer <44462289+Pandoradoomer@users.noreply.github.com>
2023-03-01 17:22:52 +0000

    [LO-84] DialogueManager functionality
    Dialogue Manager now properly:
    - fades character in and out
    - places text boxes in and out
    - places a background blur and removes it

--- Assets/Code/Managers/DialogueManager/EventPackets/StartDialoguePacket.cs
+++ Assets/Code/Managers/DialogueManager/EventPackets/StartDialoguePacket.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartDialogueEvent : IEventPacket
+public class StartDialoguePacket : IEventPacket
 {
     public DialogueSequenceData dialogueSequence;
 }
