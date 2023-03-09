c921136ff6b630e8a1e629f9001c902e1ff9b4cd
bobyalen <81637135+bobyalen@users.noreply.github.com>
2023-03-09 16:18:12 +0000

    Update DoorTrigger.cs

--- Assets/Code/Managers/Event Manager/DoorTrigger.cs
+++ Assets/Code/Managers/Event Manager/DoorTrigger.cs
@@ -10,19 +10,7 @@ public class DoorTrigger : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            //int coins = { Singleton.Instance.Inventory.GetCoins() };
-            //Debug.Log(coins);
-            /*
-        if (Inventory.instance.HasCoin(item_coin))
-            {
-                //Inventory.instance.Remove(item_coin, coins);
-                if (coins <= 0)
-                {
-                    Debug.Log("You have no coins");
-                }
-            }
             SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
-            */
         }
     }
 }
