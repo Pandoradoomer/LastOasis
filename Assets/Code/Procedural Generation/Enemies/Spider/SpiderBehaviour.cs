5c50da8a86e37dd99659d6a0e5c0a0b5e9624949
Pandoradoomer <44462289+Pandoradoomer@users.noreply.github.com>
2023-03-13 10:57:09 +0000

    [LO-110] Spider Scriptable + Script Migration

--- Assets/Code/Procedural Generation/Enemies/Spider/SpiderBehaviour.cs
+++ Assets/Code/Procedural Generation/Enemies/Spider/SpiderBehaviour.cs
@@ -4,48 +4,150 @@ using UnityEngine;
 
 public class SpiderBehaviour : BaseMoveAndAttackBehaviour
 {
-    protected override void AddAdditionalEventListeners()
-    {
-        throw new System.NotImplementedException();
-    }
+    [Header("Attack Properties")]
+    [SerializeField]
+    AttackBase attackHitbox;
+    public float stunTime;
+    public float windUpTime;
+    public float strikeTime;
+    
 
+    //Base positions for the hitbox to use for reset/rescale
+    private float initialXPos;
+    private float initialYPos;
+    private float initialXScale;
+    private float initialYScale;
+    //Enemy Direction Facing
+    public int xDir, yDir;
+
+    public bool isChillOut = true;
+    public float chillOutTimer = 0.0f;
+    private float maxChillOutTimer = 0.0f;
+    public float chillOutDuration = 0.0f;
+    private float maxChillOutDuration = 0.0f;
+
+    //Pathfinding Variables
+    public List<GridCell> path = new List<GridCell>();
+    GridCell nextCell = null;
+    public EnemyBase eb;
+    
+    new void Start()
+    {
+        base.Start();
+        initialXScale = attackHitbox.transform.localScale.x;
+        initialYScale = attackHitbox.transform.localScale.y;
+        initialXPos = attackHitbox.transform.localPosition.x;
+        initialYPos = attackHitbox.transform.localPosition.y;
+        eb = GetComponent<EnemyBase>();
+        chillOutTimer = Random.Range(4.0f, 5.0f);
+        chillOutDuration = Random.Range(0.5f, 1.0f);
+
+    }
+    private IEnumerator Stun(float duration)
+    {
+        canMove = false;
+        for (float i = 0; i <= duration; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+        canMove = true;
+    }
     protected override void DoAct()
     {
-        throw new System.NotImplementedException();
+        if (!isAttacking && canMove)
+            TurnAttackHitbox(rb.velocity);
+        if (canMove)
+            rb.velocity = GetNextMovement();
+        else
+            rb.velocity = Vector2.zero;
     }
 
-    protected override void DoAttack()
+    void TurnAttackHitbox(Vector2 dir)
     {
-        throw new System.NotImplementedException();
-    }
+        dir = dir.normalized;
 
-    protected override void DoBeginAttack()
-    {
-        throw new System.NotImplementedException();
+        //it is closer on the x axis
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+        {
+            yDir = 0;
+            attackHitbox.transform.localScale = new Vector3(initialXScale, initialYScale, 1);
+            if (dir.x < 0)
+            {
+                attackHitbox.transform.localPosition = new Vector3(-initialXPos, initialYPos, 0);
+                xDir = -1;
+            }
+            else
+            {
+                attackHitbox.transform.localPosition = new Vector3(initialXPos, initialYPos, 0);
+                xDir = 1;
+            }
+        }
+        //closer on the y axis
+        else
+        {
+            xDir = 0;
+            attackHitbox.transform.localScale = new Vector3(initialYScale, initialXScale, 1);
+            if (dir.y < 0)
+            {
+                attackHitbox.transform.localPosition = new Vector3(initialYPos, -initialXPos, 0);
+                yDir = -1;
+            }
+            else
+            {
+                attackHitbox.transform.localPosition = new Vector3(initialYPos, initialXPos, 0);
+                yDir = -1;
+            }
+
+        }
     }
 
     protected override void DoSetAnimatorVariables()
     {
-        throw new System.NotImplementedException();
-    }
 
-    protected override void DoStopAttack()
+    }
+    protected override void OnHitAction()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(Stun(stunTime));
     }
 
     protected override Vector2 GetMovement()
     {
-        throw new System.NotImplementedException();
+        return MovementFunctions.FollowPlayer(speed, transform.position, eb.rs, path, ref nextCell);
     }
 
-    protected override void OnHitAction()
+    public void OnHitboxEntered(IEventPacket packet)
     {
-        throw new System.NotImplementedException();
+        EnemyHitboxEnteredPacket ehep = packet as EnemyHitboxEnteredPacket;
+        if (ehep.Hitbox == attackHitbox.gameObject && !isAttacking)
+        {
+            Attack();
+        }
+    }
+    protected override void DoAttack()
+    {
+        StartCoroutine(AttackFunctions.Swing(this, this, windUpTime, strikeTime));
+    }
+
+    protected override void AddAdditionalEventListeners()
+    {
+        EventManager.StartListening(Event.EnemyHitboxEntered, OnHitboxEntered);
     }
 
     protected override void RemoveAdditionalEventListeners()
     {
-        throw new System.NotImplementedException();
+
+        EventManager.StopListening(Event.EnemyHitboxEntered, OnHitboxEntered);
+    }
+
+    protected override void DoBeginAttack()
+    {
+        attackHitbox.IsAttacking = true;
+        attackHitbox.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    protected override void DoStopAttack()
+    {
+        attackHitbox.IsAttacking = false;
+        attackHitbox.gameObject.GetComponent<SpriteRenderer>().enabled = false;
     }
 }
