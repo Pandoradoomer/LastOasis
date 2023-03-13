6dc565d2d32e69feabe627a45e9b375b7446cf66
bobyalen <81637135+bobyalen@users.noreply.github.com>
2023-03-13 15:20:02 +0000

    Add files via upload

---
+++ Assets/Items/Managers/ShopManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //display number of coins player has along with mini coin image
+    void displayCoins()
+    {
+
+    }
+
+    //display NPC placeholder and display NPC text
+    void NPCDisplay()
+    {
+
+    }
+
+
+    //buy item use popup manager for confirmation
+    public void Buy()
+    {
+
+    }
+
+    //display itesms for sale
+    void shop()
+    {
+
+    }
+
+    //show item purchased
+    private void bought()
+    {
+        
+    }
+}
