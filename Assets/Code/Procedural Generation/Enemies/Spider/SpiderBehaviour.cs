2eeead0f562faa63b727b2fa875c9475c8ffa6bf
Pandoradoomer <44462289+Pandoradoomer@users.noreply.github.com>
2023-03-13 10:28:44 +0000

    Spider prefab + base script

---
+++ Assets/Code/Procedural Generation/Enemies/Spider/SpiderBehaviour.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBehaviour : BaseMoveAndAttackBehaviour
+{
+    protected override void AddAdditionalEventListeners()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void DoAct()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void DoAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void DoBeginAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void DoSetAnimatorVariables()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void DoStopAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override Vector2 GetMovement()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnHitAction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void RemoveAdditionalEventListeners()
+    {
+        throw new System.NotImplementedException();
+    }
+}
