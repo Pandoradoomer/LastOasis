8b5c131c926932a4464ed4a8366f8ca5603ff41b
Pandoradoomer <44462289+Pandoradoomer@users.noreply.github.com>
2023-03-18 19:48:07 +0000

    [LO-89] Fungus knockback + Better Timer

--- Assets/Code/Procedural Generation/Enemies/Fungus/FungusBehaviour.cs
+++ Assets/Code/Procedural Generation/Enemies/Fungus/FungusBehaviour.cs
@@ -3,22 +3,41 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
+public enum FungusState
+{
+    MOVING,
+    FARTING,
+    STUN
+}
 public class FungusBehaviour : BaseMoveAndAttackBehaviour
 {
     [Header("Attack Variables")]
     [SerializeField]
     FungusAttack attackHitbox;
-    public float stunTime;
     public float windUpTime;
     public float strikeTime;
 
+    [Header("Timer Variables")]
     public float timer;
+    public float maxDistanceCap;
+    public float maxDistanceDeplete;
+    public float minDistanceCap;
+    public float minDistanceDeplete;
     private float maxTimer;
+
+
+    [Header("Push back properties")]
+    public float time;
+    public float magnitude;
+    public float stunTime;
+    public float knockBackTime;
 
     [Header("Pathfinding Variables")]
     public List<GridCell> path = new List<GridCell>();
     GridCell nextCell = null;
     public EnemyBase eb;
+
+    FungusState currState = FungusState.MOVING;
 
     // Start is called before the first frame update
     new void Start()
@@ -32,14 +51,20 @@ public class FungusBehaviour : BaseMoveAndAttackBehaviour
 
     protected override void DoAct()
     {
-        if (canMove && !isAttacking)
-            rb.velocity = GetMovement();
-        else
-            rb.velocity = Vector2.zero;
+        rb.velocity = GetMovement();
         if (timer > 0)
-            timer -= Time.deltaTime;
+            timer -= GetDepletionTime();
         else if (!isAttacking)
             DoAttack();
+    }
+    float GetDepletionTime()
+    {
+        float sqDistance = (this.transform.position - Singleton.Instance.PlayerController.transform.position).sqrMagnitude;
+        float sqMax = maxDistanceCap * maxDistanceCap;
+        float sqMin = minDistanceCap * minDistanceCap;
+        sqDistance = Mathf.Clamp(sqDistance, sqMin, sqMax);
+
+        return Time.deltaTime * Mathf.Lerp(maxDistanceDeplete, minDistanceDeplete, (sqDistance - sqMax)/(sqMin - sqMax));
     }
 
     protected override void DoAttack()
@@ -50,6 +75,7 @@ public class FungusBehaviour : BaseMoveAndAttackBehaviour
 
     protected override void DoBeginAttack()
     {
+        currState = FungusState.FARTING;
         attackHitbox.GetComponent<Collider2D>().enabled = true;
         attackHitbox.IsAttacking = true;
         attackHitbox.GetComponent<SpriteRenderer>().enabled = true;
@@ -63,17 +89,53 @@ public class FungusBehaviour : BaseMoveAndAttackBehaviour
         attackHitbox.StopAttack();
         isAttacking = false;
         attackHitbox.IsAttacking = false;
+        if(currState != FungusState.STUN)
+            currState = FungusState.MOVING;
         timer = maxTimer;
     }
 
     protected override Vector2 GetMovement()
     {
-        return MovementFunctions.FollowPlayer(speed, transform.position, eb.rs, path, ref nextCell);
+        switch(currState)
+        {
+            case FungusState.MOVING:
+                return MovementFunctions.FollowPlayer(speed, transform.position, eb.rs, path, ref nextCell);
+            case FungusState.STUN:
+                return rb.velocity;
+            case FungusState.FARTING:
+                return Vector2.zero;
+        }
+        return Vector2.zero;
     }
 
     protected override void OnHitAction()
     {
+        if(currState != FungusState.FARTING)
+            StartCoroutine(PushBack());
+    }
 
+    IEnumerator PushBack()
+    {
+        currState = FungusState.STUN;
+        canMove = false;
+        DoStopAttack();
+        Vector2 spiderPos = transform.position;
+        Vector2 playerPos = Singleton.Instance.PlayerController.transform.position;
+        Vector2 dir = (spiderPos - playerPos).normalized;
+
+        rb.velocity = Vector2.zero;
+        rb.AddForce(dir * magnitude, ForceMode2D.Impulse);
+        for (float i = 0; i <= knockBackTime; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+        rb.velocity = Vector2.zero;
+        for (float i = 0; i <= stunTime; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+        canMove = true;
+        currState = FungusState.MOVING;
     }
 
     protected override void DoSetAnimatorVariables()
